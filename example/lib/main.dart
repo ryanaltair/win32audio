5018d0893fb50505e839a26436e9f56b1f47dc8d
ryanaltair <al_tair@live.com>
2025-06-11 12:59:46 +0800

    cleanup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:win32audio/win32audio.dart';
-import 'widgets/animated_progress_bar.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,12 +24,10 @@ class _MyAppState extends State<MyApp> {
   AudioDevice defaultDevice = AudioDevice();
   List<AudioDevice> audioDevices = <AudioDevice>[];
   AudioDeviceType audioDeviceType = AudioDeviceType.output;
-  final WinIcons winIcons = WinIcons();
 
-  List<ProcessVolume> mixerList = <ProcessVolume>[];
   bool _stateFetchAudioMixerPeak = true;
 
-  double __volume = 0.0;
+  final double __volume = 0.0;
   String fetchStatus = "";
   @override
   void initState() {
@@ -41,13 +39,6 @@ class _MyAppState extends State<MyApp> {
     fetchAudioDevices();
     Timer.periodic(const Duration(milliseconds: 150), (Timer timer) async {
       if (_stateFetchAudioMixerPeak) {
-        mixerList = await Audio.enumAudioMixer() ?? <ProcessVolume>[];
-
-        for (ProcessVolume mixer in mixerList) {
-          if (_audioIcons[mixer.processPath] == null) {
-            _audioIcons[mixer.processPath] = await WinIcons().extractFileIcon(mixer.processPath);
-          }
-        }
         setState(() {});
       }
     });
@@ -61,21 +52,10 @@ class _MyAppState extends State<MyApp> {
   Future<void> fetchAudioDevices() async {
     if (!mounted) return;
     audioDevices = await Audio.enumDevices(audioDeviceType) ?? <AudioDevice>[];
-    __volume = await Audio.getVolume(audioDeviceType);
-    defaultDevice = (await Audio.getDefaultDevice(audioDeviceType))!;
 
     _audioIcons = <String, Uint8List>{};
     for (AudioDevice audioDevice in audioDevices) {
-      if (_audioIcons[audioDevice.id] == null) {
-        _audioIcons[audioDevice.id] = await WinIcons().extractFileIcon(audioDevice.iconPath, iconID: audioDevice.iconID);
-      }
-    }
-
-    mixerList = await Audio.enumAudioMixer() ?? <ProcessVolume>[];
-    for (ProcessVolume mixer in mixerList) {
-      if (_audioIcons[mixer.processPath] == null) {
-        _audioIcons[mixer.processPath] = await WinIcons().extractFileIcon(mixer.processPath);
-      }
+      if (_audioIcons[audioDevice.id] == null) {}
     }
 
     setState(() {
@@ -131,15 +111,16 @@ class _MyAppState extends State<MyApp> {
                                     defaultDevice.name,
                                     textAlign: TextAlign.justify,
                                     style: const TextStyle(color: Colors.black),
-                                    strutStyle: const StrutStyle(forceStrutHeight: true),
+                                    strutStyle: const StrutStyle(
+                                        forceStrutHeight: true),
                                   ),
                                 ),
                               ),
-                              const Icon(Icons.swap_vertical_circle_outlined, color: Colors.black),
+                              const Icon(Icons.swap_vertical_circle_outlined,
+                                  color: Colors.black),
                             ],
                           ),
                           onPressed: () async {
-                            await Audio.switchDefaultDevice(audioDeviceType);
                             fetchAudioDevices();
                             setState(() {});
                           }),
@@ -183,23 +164,7 @@ class _MyAppState extends State<MyApp> {
                 ),
               ),
             ),
-            //? VOLUMESCROLLER
-            Flexible(
-              //SLIDER
-              flex: 1,
-              fit: FlexFit.tight,
-              child: Slider(
-                value: __volume,
-                min: 0,
-                max: 1,
-                divisions: 25,
-                onChanged: (double e) async {
-                  await Audio.setVolume(e.toDouble(), audioDeviceType);
-                  __volume = e;
-                  setState(() {});
-                },
-              ),
-            ),
+
             //? LIST AUDIO DEVICES
             Flexible(
                 flex: 3,
@@ -208,19 +173,22 @@ class _MyAppState extends State<MyApp> {
                     itemCount: audioDevices.length,
                     itemBuilder: (BuildContext context, int index) {
                       return ListTile(
-                        leading: (_audioIcons.containsKey(audioDevices[index].id))
-                            ? Image.memory(
-                                _audioIcons[audioDevices[index].id] ?? Uint8List(0),
-                                width: 32,
-                                height: 32,
-                                gaplessPlayback: true,
-                              )
-                            : const Icon(Icons.spoke_outlined),
+                        leading:
+                            (_audioIcons.containsKey(audioDevices[index].id))
+                                ? Image.memory(
+                                    _audioIcons[audioDevices[index].id] ??
+                                        Uint8List(0),
+                                    width: 32,
+                                    height: 32,
+                                    gaplessPlayback: true,
+                                  )
+                                : const Icon(Icons.spoke_outlined),
                         title: Text(audioDevices[index].name),
                         trailing: IconButton(
-                          icon: Icon((audioDevices[index].isActive == true ? Icons.check_box_outlined : Icons.check_box_outline_blank)),
+                          icon: Icon((audioDevices[index].isActive == true
+                              ? Icons.check_box_outlined
+                              : Icons.check_box_outline_blank)),
                           onPressed: () async {
-                            await Audio.setDefaultDevice(audioDevices[index].id);
                             fetchAudioDevices();
                             setState(() {});
                           },
@@ -245,75 +213,6 @@ class _MyAppState extends State<MyApp> {
                 },
               ),
             ),
-            //? LIST AUDIO MIXER
-            Flexible(
-              flex: 3,
-              child: ListView.builder(
-                  itemCount: mixerList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(
-                      children: <Widget>[
-                        const Divider(
-                          height: 6,
-                          thickness: 3,
-                          color: Color.fromARGB(10, 0, 0, 0),
-                          indent: 100,
-                          endIndent: 100,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Flexible(
-                                child: ListTile(
-                                    leading: (_audioIcons.containsKey(mixerList[index].processPath))
-                                        ? Image.memory(
-                                            _audioIcons[mixerList[index].processPath] ?? Uint8List(0),
-                                            width: 32,
-                                            height: 32,
-                                            gaplessPlayback: true,
-                                          )
-                                        : const Icon(Icons.spoke_outlined),
-                                    title: Text(mixerList[index].processPath))),
-                            Flexible(
-                              child: Padding(
-                                padding: const EdgeInsets.only(right: 20),
-                                child: Column(
-                                  children: <Widget>[
-                                    Slider(
-                                      value: mixerList[index].maxVolume,
-                                      min: 0,
-                                      max: 1,
-                                      divisions: 25,
-                                      onChanged: (double e) async {
-                                        await Audio.setAudioMixerVolume(mixerList[index].processId, e.toDouble());
-                                        mixerList[index].maxVolume = e;
-                                        setState(() {});
-                                      },
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(horizontal: 25),
-                                      child: FAProgressBar(
-                                        currentValue: mixerList[index].peakVolume * mixerList[index].maxVolume * 100,
-                                        size: 12,
-                                        maxValue: 100,
-                                        changeColorValue: 100,
-                                        changeProgressColor: Colors.pink,
-                                        progressColor: Colors.lightBlue,
-                                        animatedDuration: const Duration(milliseconds: 300),
-                                        direction: Axis.horizontal,
-                                        verticalDirection: VerticalDirection.up,
-                                        formatValueFixed: 2,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    );
-                  }),
-            )
           ],
         ),
       ),
